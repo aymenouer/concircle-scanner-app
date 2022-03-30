3b6b58fc37cfd4d41ce3c4e6acc09bcdbf6bee8f
Aymen Ouerghui <aymen.ouerghui@esprit.tn>
2022-03-30 22:52:57 +0200

    add image of phones

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -18,7 +18,7 @@ export default function Intro() {
         <div className="intro" id="intro" >
             <div className="left">
                 <div className="imgContainer">
-                    <img src="assets/iphone.png" alt="" />
+                    <img src="assets/phones.png" alt="" />
                 </div>
             </div>
             <div className="right">
